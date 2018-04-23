1517d799e7c9750ccfee9b6de4cad1f7f11a6464
wjZhan <13257033208@163.com>
2018-04-23 10:08:33 +0800

    c

--- src/components/companyop.vue
+++ src/components/companyop.vue
@@ -1,20 +1,78 @@
 <template>
     <div>
-        <mt-header fixed title="选择或创建一个机构"></mt-header>
-        <mt-button type="primary" size="large" @click.native="toLogin">找到你的机构</mt-button>
-        <mt-field label="机构编号" type="password" name="age" id="age" placeholder="请输入编号" v-model="password"></mt-field>
+        <mt-header fixed title="员工管理"></mt-header>
+        <mt-button type="primary" size="large" v-if="isAdmin != 0" @click.native="addBox" >添加员工</mt-button>
+        <mt-cell  :key="index" v-for="(d,index) in list" :title="d.name" style="width:100%">
+          <mt-button type="primary" v-if="isAdmin != 0" size="small" @click.native="del(d.relId)">删除</mt-button>
+        </mt-cell>
+        <mt-popup
+        v-model="addVisible"
+        position="right">
+        <mt-field label="员工账号" placeholder="请输入员工账号" v-model="account_add"></mt-field>
+        <mt-button type="primary" size="large" @click.native="add">添加</mt-button>
+      </mt-popup>
     </div>
 </template>
 
 <script>
 import companys from "@/data/companys";
 export default {
+  created() {
+    this.isAdmin = this.$store.state.user.isAdmin;
+    this.loadData();
+  },
   data() {
     return {
-      companys
+      isAdmin: 0,
+      addVisible: false,
+      list: [],
+      account_add: ""
     };
   },
-  methods: {}
+  methods: {
+    addBox() {
+      this.addVisible = true;
+    },
+    del(id) {
+      this.$ajax({
+        method: "post",
+        url: "/api/wms/user/delUserFromDept",
+        data: "id=" + id
+      })
+        .then(res => {
+          this.loadData();
+        })
+        .catch.loadData(err => {
+          console.log(err);
+        });
+    },
+    add() {
+      this.ajax({
+        method: "post",
+        url: "/api/wms/user/joinToDept",
+        data: "account=" + account_add
+      })
+        .then(res => {
+          this.loadData();
+        })
+        .catch.loadData(err => {
+          console.log(err);
+        });
+    },
+    loadData() {
+      this.$ajax({
+        method: "post",
+        url: "/api/wms/user/queryByDeptId",
+        data: "deptId=" + this.$store.state.user.deptId
+      })
+        .then(res => {
+          this.list = res.data;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
 };
 </script>   
 
--- src/components/personalmgr.vue
+++ src/components/personalmgr.vue
@@ -4,12 +4,19 @@
     <mt-cell title="昵称" v-bind:value="store.state.user.name"></mt-cell>
     <mt-cell title="账户" v-bind:value="store.state.user.account"></mt-cell>
    <!--  <mt-cell title="邮箱" v-bind:value="store.state.user.email" v-model="test"></mt-cell> -->
-    <mt-cell title="机构" v-bind:value="dept.name"></mt-cell>
+    <mt-cell title="机构" v-bind:value="dept.name" v-if="dept != 0"></mt-cell>
     <mt-cell title="角色" v-if="dept != null && dept.isAdmin == 1">管理员</mt-cell>
     <mt-cell title="角色" v-if="dept != null && dept.isAdmin == 0">普通成员</mt-cell>
     <mt-button type="default" size="large">修改信息</mt-button>
-    <mt-button type="primary" size="large" v-if="dept != null">查看机构详情</mt-button>
-    <mt-button type="default" size="large" v-if="dept == null">增加您的机构或绑定您的机构s</mt-button>
+    <mt-button type="primary" size="large" v-if="dept == 0" @click.native="addBox">创建你的机构</mt-button>
+    <mt-button type="primary" size="large" v-if="dept != 0" @click.native="show_company">查看机构详情</mt-button>
+
+    <mt-popup
+        v-model="addVisible"
+        position="right">
+        <mt-field label="机构名称" placeholder="请输入机构名称" v-model="name_add"></mt-field>
+        <mt-button type="primary" size="large" @click.native="add">添加</mt-button>
+      </mt-popup>
   </div>
 </template>
 <script>
@@ -17,10 +24,33 @@ import store from "@/store/store";
 
 export default {
   methods: {
-    show_company() {}
+    addBox() {
+      this.addVisible = true;
+    },
+    show_company() {
+      this.$router.push({ name: "companyop" });
+    },
+    add() {
+      this.$ajax({
+        method: "post",
+        url: "/api/wms/department/add",
+        data: {
+          name: this.name_add,
+          createBy: this.store.state.user.id
+        }
+      })
+        .then(res => {
+          this.addVisible = false;
+        })
+        .catch(err => {
+          this.addVisible = true;
+        });
+    }
   },
   data() {
     return {
+      name_add: "",
+      addVisible: false,
       store,
       dept: 0
     };
@@ -29,14 +59,15 @@ export default {
     this.$ajax({
       method: "post",
       url: "/api/wms/department/queryByUserId",
-      data:"id="+store.state.user.id
+      data: "id=" + store.state.user.id
     })
       .then(res => {
-        this.store.state.user.deptId = res.data.id
+        this.store.state.user.deptId = res.data.id;
         this.dept = res.data;
+        //this.$store.state.user.isAdmin = res.data.isAdmin;
       })
       .catch(err => {
-        this.dept = null;
+        this.dept = 0;
       });
   }
 };
